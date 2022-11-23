f77eec386934d2983755489e94a9a750c3926dc9
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-23 20:56:20 +0530

    Add files via upload

---
+++ EmployeeManagementSol/EmployeeManagement/EmployeeSalaryUC2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class EmployeeSalaryUC2
+    {
+        public static void EmployeeWage()
+        {
+            //Constants
+            int IS_FULL_TIME = 1, WagePerHrs = 20, TotalWorkingHrs;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present\n");
+                TotalWorkingHrs = 8;
+            }
+            else 
+            {
+                Console.WriteLine("Employee is absent\n");
+                TotalWorkingHrs = 0;
+            }
+            int EmpSalary = WagePerHrs * TotalWorkingHrs;
+            Console.WriteLine("Employee salary is " +EmpSalary);
+        }
+    }
+}
