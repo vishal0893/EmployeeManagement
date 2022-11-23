5844701596225fbc5a66b3663ff245d8ae40d3db
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-23 20:40:07 +0530

    Add files via upload

---
+++ EmployeeManagementSol/EmployeeManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeeManagement
+{
+    class EmployeeManagement
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Management program.");
+            EmployeeSalary.EmployeeAttandance();
+        }
+    }
+}
