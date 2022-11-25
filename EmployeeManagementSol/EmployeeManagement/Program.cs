17c9d060ca4b59dfdc88a099097de0c0be0cd983
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-25 10:20:39 +0530

    Update Program.cs

--- EmployeeManagementSol/EmployeeManagement/Program.cs
+++ EmployeeManagementSol/EmployeeManagement/Program.cs
@@ -11,7 +11,16 @@ namespace EmployeeManagement
             //EmployeeSalaryUC3.EmpSalary();
             //EmployeeSalaryUC4.EmpSalarySwitchCase();
             //EmployeeSalaryUC5.EmployeeSalary();
-            EmployeeSalaryUC6.EmpSalary();
+            //EmployeeSalaryUC6.EmpSalary();
+            //EmployeeSalaryUC7.ComputeEmpWage();
+            //EmployeeSalaryUC8.ComputeWage("Dmart", 20, 5, 40);
+            //EmployeeSalaryUC8.ComputeWage("Reliance", 30, 10, 80);
+            EmployeeSalaryUC9 Dmart = new EmployeeSalaryUC9("Dmart",20,5,40);
+            EmployeeSalaryUC9 Reliance = new EmployeeSalaryUC9("Reliance", 30, 10, 80);
+            Dmart.ComputeWage();
+            Console.WriteLine(Dmart.toString());
+            Reliance.ComputeWage();
+            Console.WriteLine(Reliance.toString());
         }
     }
 }
